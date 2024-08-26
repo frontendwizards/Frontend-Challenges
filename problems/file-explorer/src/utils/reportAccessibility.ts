eeb20ceebfa6d8b3344c6dd151eb6aca355f026a
hmellahi <hamzasenpai307@gmail.com>
2024-08-26 13:59:02 +0100

    use node.js in GAs

--- problems/file-explorer/src/utils/reportAccessibility.ts
+++ problems/file-explorer/src/utils/reportAccessibility.ts
@@ -4,7 +4,7 @@ export const reportAccessibility = async (
   App: typeof React,
   config?: Record<string, unknown>
 ): Promise<void> => {
-  if (process.env.NODE_ENV !== "production") {
+  if (false && process.env.NODE_ENV !== "production") {
     const axe = await import("@axe-core/react");
     const ReactDOM = await import("react-dom");
 
