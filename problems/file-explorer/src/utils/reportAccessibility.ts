7b197518795ecf6c6428085977702fca6ef017e1
hmellahi <hamzasenpai307@gmail.com>
2024-08-26 13:59:56 +0100

    test without "process"

--- problems/file-explorer/src/utils/reportAccessibility.ts
+++ problems/file-explorer/src/utils/reportAccessibility.ts
@@ -4,7 +4,7 @@ export const reportAccessibility = async (
   App: typeof React,
   config?: Record<string, unknown>
 ): Promise<void> => {
-  if (false && process.env.NODE_ENV !== "production") {
+  if (false) {
     const axe = await import("@axe-core/react");
     const ReactDOM = await import("react-dom");
 
