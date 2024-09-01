a6753f05acc9a0f15b3d186737c69298e435057d
hmellahi <hamzasenpai307@gmail.com>
2024-09-01 02:57:35 +0100

    cleaning

---
+++ problems/file-explorer/solutions/react-ts/README.md
@@ -0,0 +1,38 @@
+# starter
+
+This project was bootstrapped with [Vite](https://vitejs.dev/) using the React TypeScript template.
+
+## Getting Started
+
+1. Clone the repository
+2. Install dependencies
+3. Start the development server
+
+## Available Scripts
+
+- `npm run dev`: Starts the development server
+- `npm run build`: Builds the app for production
+- `npm run preview`: Locally preview the production build
+
+## Tech Stack
+
+- React
+- TypeScript
+- Vite
+- Tailwind CSS
+
+## Project Structure
+
+The project structure follows Vite's default setup for React with TypeScript.
+
+## Learn More
+
+To learn more about the technologies used in this project, check out the following resources:
+
+- [Vite Documentation](https://vitejs.dev/guide/)
+- [React Documentation](https://reactjs.org/docs/getting-started.html)
+- [TypeScript Documentation](https://www.typescriptlang.org/docs/)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -18,8 +18,8 @@ PROJECT_PATH=${2:-problems}
 # Create the full path where the project will be created
 FULL_PATH="$PROJECT_PATH/$PROJECT_NAME"
 
-# Copy the starter project to the specified directory with the project name
-cp -r starter "$FULL_PATH"
+# Copy the starter project to the specified directory with the project name, excluding node_modules
+rsync -av --progress starter/ "$FULL_PATH" --exclude node_modules
 
 # Replace all instances of "starter" with the project name in package.json files
 find "$FULL_PATH/solutions/react-ts" -name 'package.json' -exec sed -i '' -e "s/starter/$PROJECT_NAME/g" {} +
@@ -27,4 +27,12 @@ find "$FULL_PATH/solutions/react-ts" -name 'package.json' -exec sed -i '' -e "s/
 # Replace "Example Challenge" with the formatted challenge name in README.md files
 find "$FULL_PATH" -name 'README.md' -exec sed -i '' -e "s/Example Challenge/$CHALLENGE_NAME/g" {} +
 
+# Change to the project directory
+cd "$FULL_PATH/solutions/react-ts"
+
+# Install dependencies and start the development server
+npm install
+
 echo "Project '$PROJECT_NAME' created successfully at '$FULL_PATH'."
+
+# try source
